842253bc19fad52d0a98900e410e8ee9f3b2b7ae
anthonyesguerra <boop@gmail.com>
2017-02-20 22:51:13 -0500

    .

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,7 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+$('#fuck').click(function(){
+  $("body").addClass("shake-slow shake-constant");
+})
